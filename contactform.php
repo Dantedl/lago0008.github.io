d352aa50baa1e84768d6c69aa95b9516ab3e9ef1
Daniel <39098553+Dantedl@users.noreply.github.com>
2019-11-22 23:41:06 -0500

    contactform/diamond grid
    php/html contact form was created and diamond grid was removed

---
+++ contactform.php
@@ -0,0 +1,20 @@
+<?php
+if(isset($_POST['submit'])){
+
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+
+    $mailTo = "lago0008@algonquinlive.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have rerecieved an e-mail from ".$name.".\n\n".$message;
+    
+    
+    mail(
+        $mailTo, $subject, $txt, $headers
+    );
+    header("Location: index.php?mailsend");
+}
+
+?>
